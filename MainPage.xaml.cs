e5dc8a1107569e4ff7aa04a768342c107133419a
unknown <nickblackburn02@gmail.com>
2018-07-08 23:35:30 -0400

    all

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -28,14 +28,10 @@ namespace furry_app
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        private void Button_Click(object sender, RoutedEventArgs e) { 
 
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
+            furry.Navigate(new Uri("https://aminoapps.com/c/furry-amino/home/"));
         }
     }
-}
+    }
+
